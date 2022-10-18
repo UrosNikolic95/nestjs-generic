3243266baab0f672e114f808b7139095728e16fc
UrosNikolic95 <4.3.1995.urosnikolic@gmail.com>
2022-10-18 19:20:55 +0200

    export watch module

--- libs/generic/src/index.ts
+++ libs/generic/src/index.ts
@@ -2,3 +2,5 @@ export * from './crud/crud.controller';
 export * from './crud/crud.dto';
 export * from './crud/crud.interface';
 export * from './crud/crud.functions';
+export * from './watch/watch.entity';
+export * from './watch/watch.subscriber';
