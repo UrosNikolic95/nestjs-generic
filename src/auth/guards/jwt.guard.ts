935c486275dd7ded7f9dec580916c288f6022c4d
UrosNikolic95 <4.3.1995.urosnikolic@gmail.com>
2022-11-22 18:45:56 +0100

    simplify JwtGuard

--- src/auth/guards/jwt.guard.ts
+++ src/auth/guards/jwt.guard.ts
@@ -5,16 +5,9 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
 
 @Injectable()
-export class JwtGuard extends AuthGuard('jwt') {
-  canActivate(context: ExecutionContext) {
-    const request = context.switchToHttp().getRequest<Request>();
-    const user = request.user;
-    return !!user;
-  }
-}
+export class JwtGuard extends AuthGuard('jwt') {}
 
 export function JwtAuth() {
   return UseGuards(JwtGuard);
