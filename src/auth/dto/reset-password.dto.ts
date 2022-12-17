70386c7c85e83dba1a5aef0fedf50722db6e53d0
UrosNikolic95 <4.3.1995.urosnikolic@gmail.com>
2022-12-17 18:23:25 +0100

    rename user table

--- src/auth/dto/reset-password.dto.ts
+++ src/auth/dto/reset-password.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 
 export class ResetPasswordDto {
   @ApiProperty()
+  @IsString()
   oldPassword: string;
 
   @ApiProperty()
+  @IsString()
   newPassword: string;
 }
--- src/entities/user-avatar.entity.ts
+++ src/entities/user-avatar.entity.ts
@@ -1,6 +1,6 @@
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity({ name: 'user' })
+@Entity({ name: 'user_avatar' })
 export class UserAvatarEntity extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
